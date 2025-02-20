52ec1ffc8b83b04ce9ef60898be6e6852b352005
Thiago Henrique Xavier Medeiros <thihxm@gmail.com>
2025-02-19 22:12:43 -0300

    script: generate protos

--- protos/generate.sh
+++ protos/generate.sh
@@ -1,5 +1,13 @@
-python -m grpc_tools.protoc --proto_path=. --go_out=../car-rental/protos/car-rental --go_opt=Mbase.proto=github.com/thihxm/car-rental/protos/base --go_opt=paths=source_relative --go-grpc_out=../car-rental/protos/car-rental --go-grpc_opt=Mbase.proto=github.com/thihxm/car-rental/protos/base --go-grpc_opt=paths=source_relative --go_opt=default_api_level=API_OPAQUE --python_out=../travel-agency/protos/car-rental --grpc_python_out=../travel-agency/protos/car-rental car-rental.proto
+#!/bin/bash
+# Let's call this script venv.sh
+echo "Activating venv"
+source "$(dirname "$0")/../.venv/bin/activate"
 
-python -m grpc_tools.protoc --proto_path=. --go_out=../hotel-chain/protos/hotel-chain --go_opt=Mbase.proto=github.com/thihxm/hotel-chain/protos/base --go_opt=paths=source_relative --go-grpc_out=../hotel-chain/protos/hotel-chain --go-grpc_opt=Mbase.proto=github.com/thihxm/hotel-chain/protos/base --go-grpc_opt=paths=source_relative --go_opt=default_api_level=API_OPAQUE --python_out=../travel-agency/protos/hotel-chain --grpc_python_out=../travel-agency/protos/hotel-chain hotel-chain.proto
+echo "Generating car-rental"
+python -m grpc_tools.protoc --proto_path="$(dirname "$0")" --go_out="$(dirname "$0")"/../car-rental/protos/car-rental --go_opt=Mbase.proto=github.com/thihxm/car-rental/protos/base --go_opt=paths=source_relative --go-grpc_out="$(dirname "$0")"/../car-rental/protos/car-rental --go-grpc_opt=Mbase.proto=github.com/thihxm/car-rental/protos/base --go-grpc_opt=paths=source_relative --go_opt=default_api_level=API_OPAQUE --python_out="$(dirname "$0")"/../travel-agency/protos/car-rental --grpc_python_out="$(dirname "$0")"/../travel-agency/protos/car-rental car-rental.proto
 
-python -m grpc_tools.protoc --proto_path=. --python_out=../travel-agency/protos/base --grpc_python_out=../travel-agency/protos/base base.proto
+echo "Generating hotel-chain"
+python -m grpc_tools.protoc --proto_path="$(dirname "$0")" --go_out="$(dirname "$0")"/../hotel-chain/protos/hotel-chain --go_opt=Mbase.proto=github.com/thihxm/hotel-chain/protos/base --go_opt=paths=source_relative --go-grpc_out="$(dirname "$0")"/../hotel-chain/protos/hotel-chain --go-grpc_opt=Mbase.proto=github.com/thihxm/hotel-chain/protos/base --go-grpc_opt=paths=source_relative --go_opt=default_api_level=API_OPAQUE --python_out="$(dirname "$0")"/../travel-agency/protos/hotel-chain --grpc_python_out="$(dirname "$0")"/../travel-agency/protos/hotel-chain hotel-chain.proto
+
+echo "Generating base"
+python -m grpc_tools.protoc --proto_path="$(dirname "$0")" --python_out="$(dirname "$0")"/../travel-agency/protos/base --grpc_python_out="$(dirname "$0")"/../travel-agency/protos/base base.proto
