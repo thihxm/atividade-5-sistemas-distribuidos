7ed8e106ca17a4793384ad3e9a7f85c70e893f2b
Thiago Henrique Xavier Medeiros <thihxm@gmail.com>
2025-02-19 21:54:50 -0300

    feat: protos

---
+++ protos/generate.sh
@@ -0,0 +1,5 @@
+python -m grpc_tools.protoc --proto_path=. --go_out=../car-rental/protos/car-rental --go_opt=Mbase.proto=github.com/thihxm/car-rental/protos/base --go_opt=paths=source_relative --go-grpc_out=../car-rental/protos/car-rental --go-grpc_opt=Mbase.proto=github.com/thihxm/car-rental/protos/base --go-grpc_opt=paths=source_relative --go_opt=default_api_level=API_OPAQUE --python_out=../travel-agency/protos/car-rental --grpc_python_out=../travel-agency/protos/car-rental car-rental.proto
+
+python -m grpc_tools.protoc --proto_path=. --go_out=../hotel-chain/protos/hotel-chain --go_opt=Mbase.proto=github.com/thihxm/hotel-chain/protos/base --go_opt=paths=source_relative --go-grpc_out=../hotel-chain/protos/hotel-chain --go-grpc_opt=Mbase.proto=github.com/thihxm/hotel-chain/protos/base --go-grpc_opt=paths=source_relative --go_opt=default_api_level=API_OPAQUE --python_out=../travel-agency/protos/hotel-chain --grpc_python_out=../travel-agency/protos/hotel-chain hotel-chain.proto
+
+python -m grpc_tools.protoc --proto_path=. --python_out=../travel-agency/protos/base --grpc_python_out=../travel-agency/protos/base base.proto
